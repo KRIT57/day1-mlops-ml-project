b574f6fa6ed2d049a2ae89168cae2366a031ec90
Krit Sharma <24csu106@ncuindia.edu>
2026-06-03 10:16:45 +0530

    Initial commit

---
+++ model.py
@@ -0,0 +1,16 @@
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+import joblib
+
+data = pd.read_csv('data/house_data.csv')
+
+x= data[['area']]
+y= data ['price']
+
+model = LinearRegression()
+model.fit(x,y)
+
+joblib.dump(model , 'model.pkl')
+
+print("model trained and saved as model.pkl")
+
